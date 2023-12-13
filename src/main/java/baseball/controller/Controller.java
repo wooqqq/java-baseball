86cdc118dbc974658efeda453ee6bc4a08ce8200
wooqqq <sge0225@naver.com>
2023-12-13 17:02:14 +0900

    feat: Number 클래스 생성

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,10 +1,18 @@
 package baseball.controller;
 
+import baseball.model.Number;
 import baseball.view.InputNumberView;
 
 public class Controller {
     public void run() {
+        Number number = getNumber();
+
+    }
+
+    private Number getNumber() {
         InputNumberView inputNumberView = new InputNumberView();
-        System.out.println(inputNumberView.getNumber());
+        String input = inputNumberView.getNumber();
+        int number = Integer.parseInt(input);
+        return new Number(number);
     }
 }
---
+++ src/main/java/baseball/model/Number.java
@@ -0,0 +1,13 @@
+package baseball.model;
+
+public class Number {
+    private final int number;
+
+    public Number(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
